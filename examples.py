08cf8585b34d758af44a84c6345f3bced9ce6ca1
chris <chrismarais@protonmail.com>
2018-05-26 14:58:11 +0100

    initial commit

---
+++ examples.py
@@ -0,0 +1,45 @@
+import numpy as np 
+from scipy.stats import norm 
+import matplotlib.pyplot as plt
+from info_theory import *  
+
+class Examples(object):
+    def example1():
+        """ Illustrate the KL-divergence between normal distributions """
+        x = np.linspace(-5,5,100)
+        p_x = norm.pdf(x)
+        q_x1 = norm.pdf(x-0.1)
+        q_x2 = norm.pdf(x-1)
+        q_x3  = norm.pdf(x + 1)
+        qs = [q_x1, q_x2, q_x3]
+        kl_divs = [KL_div(p_x, q) for q in qs]
+        plt.plot(x, p_x)
+        plt.plot(x, q_x1, x, q_x2, x, q_x3, linestyle = '--' )
+        plt.title("KL divergence")
+        plt.legend(['p(x)', *kl_divs])
+        plt.show()
+    
+    def example2():
+        """Computing mutual information associated with a joint distribution"""
+        eps = 1e-12
+        # define joint distribution p(x,y)
+        P_xy = np.array([
+            [1/8,  1/16, 1/32, 1/32],
+            [1/16, 1/8,  1/32, 1/32],
+            [1/16, 1/16, 1/16, 1/16],
+            [1/4,  eps,    eps,    eps ]
+        ])
+        print("joint distribution in form (y, x)")
+        print(P_xy)
+        # apriori distribution p(x) 
+        p_x = np.sum(P_xy, axis = 0)
+        # conditional distribution p(y|x)
+        P_YX = P_xy / p_x
+        print("mutual information using joint distribution: ")
+        print("I = %r" % I(P_xy))
+        print("mutual information using conditional distribution: ")
+        print("I = %r" % I2(p_x, P_YX))
+
+
+if __name__ == '__main__':
+    Examples.example2()
---
+++ info_theory.py
@@ -0,0 +1,59 @@
+import numpy as np 
+from numpy import log2
+
+def H(p_x):
+    """
+    Compute the entropy of a random variable distributed ~ p(x)
+    """
+    return -np.sum(p_x*log2(p_x))
+
+def H_cond(p_x, P_yx):
+    """ 
+    Compute the conditional entropy H(Y|X) given distributions p(x) and p(y|x)
+    """
+    return -np.sum((P_yx*log2(P_yx))@p_x)
+
+def KL_div(p_x, q_x):
+    """
+    Compute the KL-divergence between two random variables
+    D(p||q) = E_p[log(p(X)/q(X))]
+
+    p_x : defines p(x), shape (dim_X, )  
+    q_x : defines q(x), shape (dim_X, )
+    """
+    if p_x.shape != q_x.shape:
+        raise ValueError("p_x and q_x should have the same length")
+    return np.sum(p_x * log2(p_x/q_x))
+
+def I(Pxy):
+    """
+    Compute the mutual information between two random variables related by 
+    their joint distribution p(x,y)
+
+    Pxy : defines the joint distribution p(x,y), shape (dim_Y, dim_X)
+    """
+    p_x = np.sum(Pxy, axis = 0)
+    p_y = np.sum(Pxy, axis = 1)
+    return KL_div(Pxy, product(p_x, p_y))
+
+def I2(p_x, P_yx):
+    """
+    Compute the mutual information between two random variables given the 
+    conditional distribution p(y|x) and p(x)
+    
+    p_x :  defines distribution p(x), shape (dim_X,) 
+    P_yx : matrix defining p(y|x), shape (dim_Y, dim_X)  
+    """
+    return np.sum((P_yx*log2(P_yx/(P_yx@p_x)))@p_x)
+
+
+def product(p_x, p_y):
+    """
+    Compute the product distribution p(x,y) = p(x)p(y) given distributions 
+    p(x) and p(y)
+    """
+    p_x = p_x.reshape(1,-1)
+    p_y = p_y.reshape(-1,1)
+    Pxy = p_y@p_x
+    assert Pxy.shape == (p_y.shape[0], p_x.shape[1]) 
+    return Pxy
